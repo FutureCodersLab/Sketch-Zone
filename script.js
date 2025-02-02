3644425e333904744eade9795710cc60e5024bed
nomuk984 <nomundari0804@gmail.com>
2025-02-02 18:04:27 +0800

    halfway-checkpoint

---
+++ script.js
@@ -0,0 +1,53 @@
+import { drawSmooth } from "./canvas.js";
+const canvas = document.getElementById("canvas");
+
+let isDrawing = false;
+let hue = 0;
+let lastX;
+let lastY;
+let colorPicked = null;
+
+const outerRadiusInput = document.querySelector("#outer-radius input");
+const colorPickers = document.querySelector(".color-pickers");
+
+window.addEventListener("mousedown", (e) => {
+    if (e.target === canvas) {
+        isDrawing = true;
+        lastX = e.x;
+        lastY = e.y;
+    } else {
+        isDrawing = false;
+    }
+});
+
+window.addEventListener("mouseup", () => (isDrawing = false));
+
+window.addEventListener("mousemove", (e) => {
+    const outerRadius = outerRadiusInput.value;
+    if (isDrawing)
+        drawSmooth(lastX, lastY, e.x, e.y, outerRadius, hue, colorPicked);
+    lastX = e.x;
+    lastY = e.y;
+    hue += 0.5;
+});
+
+colorPickers.addEventListener("click", (e) => {
+    const colorPicker = e.target;
+    if (!colorPicker.id) return;
+    if (colorPicker.id === "solid-color") {
+        colorPicker.addEventListener(
+            "change",
+            () => (colorPicked = e.target.value)
+        );
+    }
+    if (colorPicker.id === "hue-cycling") {
+        colorPicked = "";
+        hue = 0;
+    }
+
+    document.querySelectorAll(".color-container").forEach((container) => {
+        container.classList.remove("active");
+    });
+
+    colorPicker.parentElement.classList.add("active");
+});
